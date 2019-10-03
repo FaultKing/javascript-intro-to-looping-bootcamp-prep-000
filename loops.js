9fa68f5de8e0e79f938438932ac4b4258b88beb5
Ioan S <ioan.scurtu120@gmail.com>
2019-10-03 14:25:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,18 +1,32 @@
-
-function whileLoop(num){
-  while(--num > 0){
-    console.log(num)
-  }
-  console.log(num)
-  return 'done'
-}
-
 function forLoop(array){
-  for(let i = 0; i < 25; i++){
-    var optionalS = ((i == 1) ? "" : "s")
-    console.log(`I am ${i} strange loop${optionalS}`)
-    array.push(`I am ${i} strange loop${optionalS}`)
+  for(let i = 0; i < 25; i++ ){
+    console.log(`I am ${i} strange loop${i === 1 ? '': 's'  }`);
+    if(i === 1 ){
+      array.push(`I am ${i} strange loop.`)
+    } else{
+      array.push(`I am ${i} strange loops.`)
+    }
   }
   return array
 }
-/* I don't know :( :((
+
+  function whileLoop(num){
+    var n = num
+
+    while( n > 0){
+      --n // n = n - 1
+      console.log(n)
+    }
+    return 'done'
+  }
+
+  function doWhileLoop(integer){
+    i=0
+    function incrementVariable() {
+      i = i + 1;
+      return i;
+    }
+    do {
+      console.log("I run once regardless.");
+    } while (incrementVariable() < integer);
+  };
